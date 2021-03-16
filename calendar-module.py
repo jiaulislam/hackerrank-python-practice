fae82166af4a64e12c63ee366b16b1fdfb6fa86d
w4nn4b3cod3r <kmjiaulislam@gmail.com>
2021-03-16 16:45:26 +0600

    Solve

---
+++ calendar-module.py
@@ -0,0 +1,13 @@
+import calendar
+
+def get_day_name(year, month, day):
+    days = ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY']
+    weekday = calendar.weekday(year, month, day)
+    # print(weekday)
+    return days[weekday]
+        
+
+
+if __name__=="__main__":
+    month, day, year = map(int, input().split())
+    print(get_day_name(year, month, day))
